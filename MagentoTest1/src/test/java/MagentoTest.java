2ac9d792d0bc9f25926da214073c772078342319
pankaj parmar <pankaj parmar@LAPTOP-B8QSIST2>
2018-10-13 12:25:38 +0530

    1st push

---
+++ MagentoTest1/src/test/java/MagentoTest.java
@@ -0,0 +1,36 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MagentoTest {
+
+public void posCredentials() throws Exception {
+
+		String url = "http://www.magento.com";
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get(url);
+
+		Main m = new Main(driver);
+		m.clickOnMyAcc();
+		Thread.sleep(3000);
+
+		Login login = new Login(driver);
+		Thread.sleep(3000);
+		login.typeEmail("sucheendra.abc@gmail.com");
+		Thread.sleep(3000);
+		login.typePwd("Welcome123");
+		Thread.sleep(3000);
+		login.clickOnLogin();
+
+		LogOut logout = new LogOut(driver);
+		logout.clickOnLogOut();
+		Thread.sleep(3000);
+
+	}
+
+}
